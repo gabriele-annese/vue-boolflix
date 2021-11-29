f8b37f147929765caf4f8846da66b67671190dcb
gabriele-annese <gannese06@gmail.com>
2021-11-29 16:49:59 +0100

    add flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,12 +4,22 @@
             <h2>
                 Titolo: {{title}}
             </h2>
+      </li>
+      <li>
             <h4>
                 Titolo Originale: {{originalTitle}}
             </h4>
-            <h4>
+      </li>
+      <li class="img-container">
+          <h4>
+              Lingua:
+          </h4>
+            <img v-if="flagVisible" :src="require(`../assets/${this.language}.png`)" :alt="language">
+            <span  v-else>
                 Lingua: {{language}}
-            </h4>
+            </span>
+      </li>
+      <li>
             <h4>
                 Valutazione: {{text}}
             </h4>
@@ -25,6 +35,14 @@ props:{
     originalTitle: String,
     language: String,
     text: Number,
+},
+computed:{
+    flagVisible(){
+        let visible = false
+        if(this.language === "it" || this.language === "it-IT") visible= true;
+        if (this.language === "en-EN" || this.language === "en") visible= true
+        return visible;
+    },
 }
 }
 </script>
@@ -33,8 +51,22 @@ props:{
 ul {
     margin-top: 20px;
     margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    li{
+        list-style: none;
+    }
     h2,h4{
         padding-top: 8px;
+    }
+    .img-container{
+       display: flex;
+       align-items: center;
+    }
+    img{
+        margin: 5px 8px;
+        width: 50px;
+        height: 30px;
     }
 }
 </style>
