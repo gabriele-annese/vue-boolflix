a16798347a03975083e4dca02b4ba5e9cec1b3d3
gabriele-annese <gannese06@gmail.com>
2021-11-30 12:40:01 +0100

    add bootstrap

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,11 +1,11 @@
 <template>
-    <section>
-        <div class="box-movies"  v-if = "moviesArray !== null">
+    <section >
+        <div  class="box-movies" v-if = "moviesArray !== null">
             <div v-for="(elMovies, index) in moviesArray" :key="`elMovies-${index}`" >
                 <div class="imgMovie" >
                      <img :src="`https://image.tmdb.org/t/p/${Imgsize}${elMovies.poster_path} `" :alt="elMovies.title">
                 </div>
-                <Card 
+                <Card class="card"
                 :title="elMovies.title"
                 :originalTitle="elMovies.original_title"
                 :language="elMovies.original_language"
@@ -13,8 +13,8 @@
                 />
             </div>
         </div>
-         <div class="box-tv"  v-if = "tvArray !== null">
-            <div v-for="(elTv, index) in tvArray" :key="`elTv-${index}`" >
+         <div   class="box-tv" v-if = "tvArray !== null">
+            <div v-for="(elTv, index) in tvArray" :key="`elTv-${index}`">
              <div class="imgMovie" >
                      <img :src="`https://image.tmdb.org/t/p/${Imgsize}${elTv.poster_path} `" :alt="elTv.name">
                 </div>
